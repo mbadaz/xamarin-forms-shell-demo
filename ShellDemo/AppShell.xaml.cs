a21399f9f37b52a29dc0b34cc78cefb343d4da40
tapsmuzira@gmail.com <LoveMyMother3908>
2020-06-01 11:41:02 +0200

    First commit

---
+++ ShellDemo/AppShell.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ShellDemo
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            FlyoutIsPresented = false;
+            DisplayAlert("Rate!", "You selected the Rate This App item", "Ok");
+        }
+    }
+}
---
+++ ShellDemo/Views/PageTwo.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ShellDemo.Views
+{
+    public partial class PageTwo : ContentPage
+    {
+        public PageTwo()
+        {
+            InitializeComponent();
+        }
+    }
+}
